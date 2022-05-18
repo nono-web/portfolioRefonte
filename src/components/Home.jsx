6f4a1d060c8afa3220e80a7e5780c4bcf267ea40
[REM-FR]Arnaud Didier <84334523+nono-web@users.noreply.github.com>
2022-05-18 10:35:24 +0200

    home

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -5,7 +5,7 @@ import { mobile } from '../responsive';
 
 const Container = styled.div`
   background-image: url(${imgHome});
-  height: 85vh;
+  height: 80vh;
   display: flex;
   flex-direction:  column;
   justify-content: center;
